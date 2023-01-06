4a96073c1a0cd9b64a64b1b4593dfe5e0743af8a
Alan Jahir <alan.martinez5691@alumnos.udg.mx>
2023-01-06 00:19:10 -0600

    Pagina de casos

--- src/containers/pages/Cases.jsx
+++ src/containers/pages/Cases.jsx
@@ -1,3 +1,5 @@
+import CasesList from "components/cases/CasesList";
+import Header from "components/cases/Header";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import Layout from "hocs/layouts/Layout";
@@ -8,7 +10,8 @@ function Cases()
         <Layout>
             <Navbar/>
                 <div className="pt-28">
-                    Cases    
+                    <Header/>
+                    <CasesList/>
                 </div>
             <Footer/>
         </Layout>
